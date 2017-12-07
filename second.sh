da5ccf14934a096f3e504fdb9f8797c4e2e2bb70
mkhuda <mhmmd.huda@gmail.com>
2017-12-08 03:01:12 +0700

    second.sh tutorial

---
+++ second.sh
@@ -0,0 +1,10 @@
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
